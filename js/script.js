aaed195244c2e01d1f8cc544885da3a8182e7683
Suneel Kumar <suneel.kumar@kiwitech.com>
2025-04-11 21:07:45 +0530

    Home Page UI

--- js/script.js
+++ js/script.js
@@ -4,16 +4,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
   window.addEventListener('scroll', function () {
     const scrolled = window.scrollY || document.documentElement.scrollTop;
-    header?.classList.toggle('fixed-header', scrolled > 100);
+    header?.classList.toggle('fixed-header', scrolled > 10);
   });
 });
 
-// Slider configuration
-const scrollElement = document.getElementById('myScroll');
-document.getElementById('prevBtn').addEventListener('click', () => {
-  scrollElement.scrollLeft -= scrollElement.offsetWidth;
+// Profile Slider Settings
+const swiper = new Swiper(".profile-slider", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  mousewheel: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
 });
 
-document.getElementById('nextBtn').addEventListener('click', () => {
-  scrollElement.scrollLeft += scrollElement.offsetWidth;
+// Testimonial Slider Settings
+const verticalSwiper = new Swiper('.review-vertical-slider', {
+  slidesPerView: 1,
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next"
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Country Select
+$("#contactNo").intlTelInput({
+	initialCountry: "in",
+	separateDialCode: true,
 });
